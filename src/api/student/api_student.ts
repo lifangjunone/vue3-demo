cbce98900f12cd714a0074471d4195f9926d5bb4
李芳军 <lifangjun@lifangjundeMacBook-Pro.local>
2022-11-06 20:05:58 +0800

    feature: add new view

--- src/api/student/api_student.ts
+++ src/api/student/api_student.ts
@@ -1,8 +1,10 @@
 import axios from "../request"
 import { testType } from "../models";
-//请求示例
-//get
-export const student = (data:any) => {
+import { json } from "stream/consumers";
+import { dataType } from "element-plus/es/components/table-v2/src/common";
+// 获取学生列表
+// get
+export const studentGet = (data:any) => {
     return axios({
         url: "/api/example_api/user",
         method: "get",
@@ -10,6 +12,53 @@ export const student = (data:any) => {
         config: {
             headers: {
                 'Content-Type': 'wechat'
+            },
+            timeout: 10000
+        }
+    })
+}
+// 创建学生
+// post
+export const studentPost = (data:any) => {
+    return axios({
+        url: "/api/example_api/user",
+        method: "post",
+        data,
+        config: {
+            headers: {
+                'Content-Type': 'wechat'
+            },
+            timeout: 10000
+        }
+    })
+}
+
+// 编辑学生
+// put
+export const studentPut = (data:any) => {
+    return axios({
+        url: "/api/example_api/user",
+        method: "put",
+        data,
+        config: {
+            headers: {
+                'Content-Type': 'wechat'
+            },
+            timeout: 10000
+        }
+    })
+}
+
+// 删除学生
+// delete
+export const studentDelete = (data:JSON) => {
+    return axios({
+        url: "/api/example_api/user",
+        method: "delete",
+        data,
+        config: {
+            headers: {
+                'Request-Type': 'application/json'
             },
             timeout: 10000
         }
