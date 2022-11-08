82b175d38b32aab17c10bc82e0953170a9520180
lifangjun <lifj@ebondhm.com>
2022-11-08 16:28:05 +0800

    feature: add new api

--- src/views/create.vue
+++ src/views/create.vue
@@ -1,65 +1,93 @@
 <template>
-    <el-dialog width="20%" v-model="dialogFormVisible" title="添加学生">
-        <el-form :model="form">
-            <el-form-item label="姓名" :label-width="formLabelWidth">
-                <el-input v-model="form.username" autocomplete="off" />
-            </el-form-item>
-            <el-form-item label="性别" :label-width="formLabelWidth">
-                <el-select v-model="form.sex" placeholder="选择性别">
-                    <el-option label="男生" value="男" />
-                    <el-option label="女生" value="女" />
-                </el-select>
-            </el-form-item>
-            <el-form-item label="年龄" :label-width="formLabelWidth">
-                <el-input v-model="form.age" autocomplete="off" />
-            </el-form-item>
-        </el-form>
-        <template #footer>
-            <span class="dialog-footer">
-                <el-button @click=closeDialog(false)>取消</el-button>
-                <el-button type="primary" @click="handleAdd">
-                    保存
-                </el-button>
-            </span>
-        </template>
-    </el-dialog>
+  <el-dialog width="20%" v-model="dialogFormVisible" title="添加学生">
+    <el-form :model="editData3">
+      <el-form-item label="姓名" :label-width="formLabelWidth">
+        <el-input v-model="editData3.username" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="性别" :label-width="formLabelWidth">
+        <el-select v-model="editData3.sex" placeholder="选择性别">
+          <el-option label="男生" value="男" />
+          <el-option label="女生" value="女" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="年龄" :label-width="formLabelWidth">
+        <el-input v-model="editData3.age" autocomplete="off" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click=closeDialog(false)>取消</el-button>
+        <el-button type="primary" @click="handleAddOrUpdate">
+          保存
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
 
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref , toRefs} from 'vue'
-import {studentPost} from '@/api/student/api_student'
-
+import { reactive, ref, toRefs } from 'vue'
+import { studentPost, studentPut } from '@/api/student/api_student'
 
 let emit = defineEmits()
+
 let closeDialog = (type) => {
-    emit('handleEmit', type)
+  emit('handleEmit', type)
 }
-const form = reactive({
-  username: '',
-  age: '',
-  sex: '',
+let editData3 = reactive({
+  username: "",
+  age: 0,
+  sex: "",
 })
 
 
 const formLabelWidth = '140px'
 // 接受父组件参数
 let props = defineProps({
-    dialogFormVisible:Boolean,
-    editData: reactive({}),
-    })
+  dialogFormVisible: Boolean,
+  isCreate: Boolean,
+  editData: Object,
 
-const {editData} = toRefs(props)
-console.log('editData', JSON.parse(JSON.stringify(editData)))
-let editData2 = editData
+});
+
+const { isCreate, editData } = toRefs(props)
+
+console.log('editData::', JSON.parse(JSON.stringify(editData)))
+
+if (isCreate.value === true) {
+  let editData2 = JSON.parse(JSON.stringify(JSON.parse(JSON.stringify(editData))))["_object"]["editData"]["value"]
+  console.log(editData2)
+  editData3 = reactive(editData2)
+}
+
+const handleAddOrUpdate = () => {
+  console.log("3333333", isCreate.value)
+  if (isCreate.value === true) {
+    handleEdit()
+
+  } else {
+    handleAdd()
+  }
+}
+
 // 添加学生接口
 const handleAdd = () => {
-  studentPost(form).then((res:any) => {
+  studentPost(editData3).then((res: any) => {
     if (res.code === 10000) {
-    //   toLoad()
+      closeDialog(true)
     }
-    closeDialog(true)
+    closeDialog(false)
+  })
+}
 
+// 编辑学生接口
+const handleEdit = () => {
+  studentPut(editData3).then((res: any) => {
+    if (res.code === 10000) {
+      closeDialog(true)
+    }
+    closeDialog(false)
   })
 }
 
@@ -70,12 +98,15 @@ const handleAdd = () => {
 .el-button--text {
   margin-right: 15px;
 }
+
 .el-select {
   width: 300px;
 }
+
 .el-input {
   width: 300px;
 }
+
 .dialog-footer button:first-child {
   margin-right: 10px;
 }
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,25 +1,26 @@
 <template>
   <div style="margin-top:200px">
     <h3 style="font-weight:bolder">学生管理系统</h3>
-    <button style="background: green" @click="dialogFormVisible = true">添加</button>
+    <button style="background: green; margin-top:50px; margin-bottom:50px" @click="dialogFormVisible = true"
+      :editData="tsData">添加</button>
 
     <el-table :data="tableObj.tableData" style="width: auto">
       <el-table-column prop="id" label="编号" width="auto" />
       <el-table-column prop="username" label="姓名" width="auto" />
       <el-table-column prop="sex" label="性别" width="auto" />
       <el-table-column prop="age" label="年龄" width="auto" />
+      <el-table-column prop="created_at" label="创建时间" width="auto" />
+      <el-table-column prop="updated_at" label="更新时间" width="auto" />
       <el-table-column fixed="right" label="操作" width="auto">
         <template #default="scope">
           <el-button link type="primary" size="small" @click="handleDelete(scope.row.id)">删除</el-button>
-          <!-- <el-button link type="primary" size="small" @click="handleEdit(scope.row.id)">编辑</el-button> -->
-          <el-button link type="primary" size="small" @click="editRow(scope.$index)" >编辑</el-button>
+          <el-button link type="primary" size="small" @click="editRow(scope.$index)">编辑</el-button>
         </template>
       </el-table-column>
     </el-table>
     <!-- 引入子页面［添加页面］ -->
-    <create v-if="dialogFormVisible" @handleEmit="handleList" v-bind:editData="editRow" v-model:dialogFormVisible="dialogFormVisible"></create>
-    
-
+    <create v-if="dialogFormVisible" @handleEmit="handleList" :editData="tsData" :isCreate="isCreate"
+      v-model:dialogFormVisible="dialogFormVisible"></create>
   </div>
 </template>
 
@@ -31,64 +32,54 @@ import create from './create.vue'
 
 
 let dialogFormVisible = ref(false)
+let isCreate = ref(false)
+let tsData = reactive({})
 
 let tableObj = reactive({
-  tableData:[],
+  tableData: [],
 })
-let editData : UserInfo
 
-interface UserInfo {
-  id: number
-  username: string
-  age: number
-  sex: string
-}
 
 // 删除学生接口
 const handleDelete = (id: number) => {
 
-  const params ={
+  const params = {
     id: id,
   }
   JSON.stringify(params)
-  studentDelete(params).then((res:any) => {
+  studentDelete(params).then((res: any) => {
     if (res.code === 10000) {
       toLoad()
     }
   })
 }
-// 编辑学生接口
-const handleEdit = (id: number) => {
-  studentPut(id).then((res:any) => {
-    if (res.code === 10000) {
-      toLoad()
-    }
-  })
-}
+
 // 显示学生列表
 const toLoad = () => {
-  studentGet({}).then((res:any) => {
+  studentGet({}).then((res: any) => {
     if (res.code === 10000) {
       tableObj.tableData = res.data
     }
   })
 }
 
-const handleList = (type)=>{
-  if(type){
+const handleList = (type) => {
+  if (type) {
     toLoad()
   }
-  dialogFormVisible.value=false
-  
+  dialogFormVisible.value = false
+  isCreate.value = false
+
 }
 const editRow = (index) => {
-  // editData = JSON.parse(JSON.stringify(tableObj.tableData[index]))
-  editData = tableObj.tableData[index]
-  console.log('curRow', editData)
+  tsData.value = JSON.parse(JSON.stringify(tableObj.tableData[index]))
+  console.log('curRow', tsData)
   dialogFormVisible.value = true
-  return editData
+  isCreate.value = true
+  return tsData
 }
-onMounted(() =>{
+
+onMounted(() => {
   toLoad()
 })
 
